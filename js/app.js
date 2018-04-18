eac5db57bb76eb300f457c78554e7c80646e345c
Cesarlagcc <cesarandon092@gmail.com>
2018-04-18 18:58:19 -0400

    updated banner

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
   $(window).scroll(function () {
       console.log($(window).scrollTop())
-    if ($(window).scrollTop() > 594) {
+    if ($(window).scrollTop() > 493) {
       $('.flex-container').addClass('navbar-fixed');
       $('body').css("margin-top","50px")
     }
-    if ($(window).scrollTop() < 598) {
+    if ($(window).scrollTop() < 495) {
       $('.flex-container').removeClass('navbar-fixed');
       $('body').css("margin-top","0")
     }
